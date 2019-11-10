2bb4d53c9f937e9e41d7b19d011ba93452d9ab8a
Rafael <rafael_guliev94@list.ru>
2019-11-11 02:14:43 +0300

    add router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React from 'react';
-import './App.css';
+import {
+    Router,
+    Switch,
+    Route,
+} from "react-router-dom";
+
+import styles from './App.scss';
 
 import Header from './components/Header/Header';
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Portfolio from "./components/Portfolio/Portfolio";
+import { createBrowserHistory } from "history";
+const customHistory = createBrowserHistory();
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-    </div>
+      <Router history={customHistory}>
+        <div className={styles.App}>
+            <Header/>
+
+            <Switch>
+                <Route exact path={'/'} component={Home}/>
+                <Route exact path={'/about'} component={About}/>
+                <Route exact path={'/portfolio'} component={Portfolio}/>
+            </Switch>
+
+        </div>
+      </Router>
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,53 @@
 import React, {Component} from 'react';
-
+import {Link} from 'react-router-dom';
 import styles from './Header.scss';
 
 export default class Header extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            burgerMenu: false
+        }
+    }
+
+    handleToggleBurgerMenu = () => {
+        this.setState({
+            burgerMenu: !this.state.burgerMenu
+        })
+    };
+
     render() {
+
+        const {burgerMenu} = this.state;
+        let burgerButton = styles.header_button;
+        let nav = null;
+
+        if (burgerMenu) {
+            burgerButton += ` ${styles.active}`;
+            nav = <nav className={styles.header_nav}>
+                <ul className={styles.header_list}>
+                    <li>
+                        <Link onClick={this.handleToggleBurgerMenu} to={'/'}>Home</Link>
+                    </li>
+                    <li>
+                        <Link onClick={this.handleToggleBurgerMenu} to={'/about'}>About</Link>
+                    </li>
+                    <li>
+                        <Link onClick={this.handleToggleBurgerMenu} to={'/portfolio'}>Portfolio</Link>
+                    </li>
+                </ul>
+            </nav>;
+        }
+
         return(
             <header className={styles.header}>
-                <div></div>
+                <button onClick={this.handleToggleBurgerMenu} className={burgerButton} type={'button'}>
+                    <span className={styles.button_top}></span>
+                    <span className={styles.button_mid}></span>
+                    <span className={styles.button_bot}></span>
+                </button>
+                {nav}
             </header>
         )
     }

bfeff3a36d371c3fcb08e145b03f511878441d4b
Rafael <rafael_guliev94@list.ru>
2019-11-10 23:12:58 +0300

    Start dev project

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import styles from './Header.scss';
+
+export default class Header extends Component {
+    render() {
+        return(
+            <header className={styles.header}>
+                <div></div>
+            </header>
+        )
+    }
+}

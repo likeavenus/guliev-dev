2f11fedb8d6ed7ab0339b57d062cf7bf673ed5a7
Rafael <37466798+likeavenus@users.noreply.github.com>
2021-11-10 00:31:57 +0300

    Updates

---
+++ precache-manifest.b61b81ef803a449ff29a19b47fa861e7.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "190a794f8fd2c41012430ed69410999d",
+    "url": "/guliev/index.html"
+  },
+  {
+    "revision": "513537fdc8dd7b83200b",
+    "url": "/guliev/static/css/main.f3dd307b.chunk.css"
+  },
+  {
+    "revision": "df21f90d60cd68bd586b",
+    "url": "/guliev/static/js/2.a53f722d.chunk.js"
+  },
+  {
+    "revision": "513537fdc8dd7b83200b",
+    "url": "/guliev/static/js/main.44ff60c2.chunk.js"
+  },
+  {
+    "revision": "36e86fe046f70dc0069f",
+    "url": "/guliev/static/js/runtime-main.6421a100.js"
+  },
+  {
+    "revision": "aedadc0026585ed139dad6c474dbbfd8",
+    "url": "/guliev/static/media/1.aedadc00.png"
+  },
+  {
+    "revision": "ff761ba2443d9c7407032555e95cfe2f",
+    "url": "/guliev/static/media/2.ff761ba2.jpg"
+  },
+  {
+    "revision": "e64f89a548efd91ce55206e89c82e384",
+    "url": "/guliev/static/media/3.e64f89a5.png"
+  },
+  {
+    "revision": "d085a6fef36270133abe71239523a795",
+    "url": "/guliev/static/media/4.d085a6fe.png"
+  },
+  {
+    "revision": "642217b3ef0f381ad13d6483c3d25cfb",
+    "url": "/guliev/static/media/5.642217b3.png"
+  },
+  {
+    "revision": "d385e5d6736c78bbe43e651488cea4fb",
+    "url": "/guliev/static/media/6.d385e5d6.png"
+  },
+  {
+    "revision": "bdafa19571613229d6a54ac038416abe",
+    "url": "/guliev/static/media/aqua.bdafa195.jpg"
+  },
+  {
+    "revision": "6b332b61ff27c70381668d847afe8224",
+    "url": "/guliev/static/media/aurora.6b332b61.png"
+  },
+  {
+    "revision": "714d08947ef6f900e48687e721b6dad0",
+    "url": "/guliev/static/media/bg2.714d0894.jpg"
+  },
+  {
+    "revision": "86d7106756af49fc3cb340da29762104",
+    "url": "/guliev/static/media/elon.86d71067.png"
+  },
+  {
+    "revision": "4347925cb1e5514063f8d90f6ccbc611",
+    "url": "/guliev/static/media/error.4347925c.png"
+  },
+  {
+    "revision": "981a9ad22d113f2d874370406385d224",
+    "url": "/guliev/static/media/gears.981a9ad2.png"
+  },
+  {
+    "revision": "19fb4011e429e329f5ba972ce3270910",
+    "url": "/guliev/static/media/html.19fb4011.png"
+  },
+  {
+    "revision": "9f2f78fd13f5757c6df332d9db44d0e7",
+    "url": "/guliev/static/media/kanban.9f2f78fd.png"
+  },
+  {
+    "revision": "d5a359d1180ecef567904c93517d8161",
+    "url": "/guliev/static/media/leto.d5a359d1.jpg"
+  },
+  {
+    "revision": "d5e9a1dfc0f8539a0d0b0d9a1455dd2c",
+    "url": "/guliev/static/media/switzerland.d5e9a1df.jpg"
+  },
+  {
+    "revision": "49aafe51fa7aacf7edcd760e7913afa6",
+    "url": "/guliev/static/media/telegram.49aafe51.svg"
+  },
+  {
+    "revision": "9ec045e1341b6283ba9780ba1a0fa573",
+    "url": "/guliev/static/media/thunder.9ec045e1.jpg"
+  },
+  {
+    "revision": "d72748d4278c48c84805ab95d7f858d6",
+    "url": "/guliev/static/media/whatsapp.d72748d4.svg"
+  }
+]);
